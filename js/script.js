770f02fd5f309dca9bbd52f7e6b678398e2e5f29
Daniar02 <abykeev02@icloud.com>
2023-01-29 17:22:22 +0600

    Практика. Задание на отработку действий со страницей

--- js/script.js
+++ js/script.js
@@ -24,6 +24,37 @@ const movieDB = {
     ]
 };
 
+const adv = document.querySelectorAll('.promo__adv img'),
+      poster = document.querySelector('.promo__bg'),
+      genre = poster.querySelector('.promo__genre'),
+      movieList = document.querySelector('.promo__interactive-list');
+
+// for (let i = 0; i < adv.length; i++) { // Удалить реклами №1
+//     adv[i].remove();
+// }
+adv.forEach(item => { // Удалить реклами №2
+    item.remove();
+});
+// adv.forEach(function(item) { // Удалить реклами №3
+//     item.remove();
+// });
+///////////////////////////////////////////////////////
+genre.textContent = "Драма"; // Изменил жанр фильма
+//////////////////////////////////////////////////////
+poster.style.backgroundImage = 'url("img/bg.jpg")'; // Изменил задний фон
+//////////////////////////////////////////////////////
+movieList.innerHTML = ""; // Очистили
+///////////////////////
+movieDB.movies.sort(); // сортировали
+////////////////////////
+movieDB.movies.forEach((film, i) => {
+    movieList.innerHTML +=  // (a = a + 1)- (a += 1); 
+    ` 
+        <li class="promo__interactive-item"> ${i + 1} ${film}
+            <div class="delete"></div>
+        </li>
+    `;                        
+});
 
 
 
